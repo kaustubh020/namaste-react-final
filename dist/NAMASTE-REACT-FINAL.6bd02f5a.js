f325abb35c4e0ce503e973c66b2bc336d9ed376a
Kaustubh Bhat <kaustubhbhat@Kaustubhs-MacBook-Air.local>
2025-09-05 23:13:43 +0530

    Stop tracking dist and .parcel-cache

--- dist/NAMASTE-REACT-FINAL.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", {}, "hello world");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=NAMASTE-REACT-FINAL.6bd02f5a.js.map
