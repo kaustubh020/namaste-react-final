c01edfbd68499c69412d486295fe41379bdd5f4f
Kaustubh Bhat <kaustubhbhat@Kaustubhs-MacBook-Air.local>
2025-09-05 22:42:49 +0530

    epi 2

---
+++ dist/NAMASTE-REACT-FINAL.6bd02f5a.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=NAMASTE-REACT-FINAL.6bd02f5a.js.map
